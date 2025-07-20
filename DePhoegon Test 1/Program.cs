c2e3c6a651f8eef133b0f4f1d3525f591e5d76fe
Kerns Noel (ASRock) <45953503+DePhoegon@users.noreply.github.com>
2025-07-19 23:50:07 -0600

    Init Logic & structure - Not finished
    Refactor Word Guess game for improved structure
    
    - Introduced constants for window dimensions and hang chances.
    - Encapsulated game initialization in a new `GameInit` method.
    - Renamed and restructured drawing functions for clarity.
    - Enhanced random word selection to allow for two-word returns.
    - Improved handling of guessed characters to prevent duplicates.
    - Overall enhancements to maintainability and functionality.

--- DePhoegon Test 1/Program.cs
+++ DePhoegon Test 1/Program.cs
@@ -1,56 +1,82 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
+
 class WordGuess {
-    static void Main() { 
+    public const int intendedWidth = 50;
+    public const int intendedHeight = 50;
+    public const int hangChances = 6;
+
+    // Game state variables (static for static methods)
+    static char[] guessedChars = [];
+    static int wordListCount = 10;
+    static string[] wList = [];
+    static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    static char[] answer = [];
+    static char[] guessedAnswer = [];
+    static int answerLength;
+    static int hangCount = 0;
+
+    static void Main() {
+        SetWindow();
         Console.WriteLine("Guess Word Game!");
-        int count = 10;
-        int hangChances = 6;
-        string[] wList;
-        char[] letters;
-        string answer;
-        string guessedAnswer;
-        int answerLength;
         
         // Initialize the game
-        gameInit(out answer, out answerLength, out guessedAnswer, out wList, out letters, count, hangChances);
+        GameInit();
 
-        drawMan(answer, letters, hangChances);
-        setWindow();
+        DrawBoard(answer, alphabet, 0);
 
-        while (/* game not Over */) { ... }
-
-        
+        while (hangCount < hangChances) {
+            // get user input
+            DrawBoard(answer, alphabet, hangCount);
+            // wait for input
+            char input = Console.ReadKey(true).KeyChar;
+            input = char.ToLowerInvariant(input);
+            if (SetGuessedChars(input)) {
+                if (lettersGuessed(input)) { Console.WriteLine($"Good guess! '{input}' is in the word!"); }
+                else {
+                    hangCount++;
+                    Console.WriteLine($"Sorry, '{input}' is not in the word. You have {hangChances - hangCount} chances left.");
+                }
+            } else {
+                Console.WriteLine("You already guessed that letter! Try again.");
+                continue; 
+            }
+        }
     }
-    static void setWindow() {
+    private static bool lettersGuessed(char guessed) {
+        bool found = false;
+        for (int i = 0; i < answer.Length; i++) {
+            if (char.ToLowerInvariant(answer[i]) == guessed && guessedAnswer[i] == '_') {
+                found = true;
+                guessedAnswer[i] = guessed;
+            }
+        }
+        if (!found && guessed != ' ') { hangCount++; }
+        return found;
+    }
+    static void SetWindow() {
         if (OperatingSystem.IsWindows()) {
-            Console.SetWindowSize(50, 50);
-            Console.BufferHeight = 50;
-            Console.BufferWidth = 50;
+            Console.SetWindowSize(width: intendedWidth, height: intendedHeight);
+            Console.BufferHeight = intendedHeight;
+            Console.BufferWidth = intendedWidth;
         }
-        if (OperatingSystem.IsMacOS() || OperatingSystem.IsLinux()) { Console.Write("\x1b[8;50;50t"); }
+        if (OperatingSystem.IsMacOS() || OperatingSystem.IsLinux()) { Console.Write($"\x1b[8;{intendedHeight};{intendedWidth}t"); }
     }
-    static void gameInit(out string selection, out int selectionLength, out string guessed, out string[] wordlist, out char[] alphabet, int count, int hangs)
-    {
-        setWordList(out wordlist, out alphabet, count);
+    static void GameInit() {
+        SetWordList();
         Random random = new();
-        selection = wordlist[random.Next(0, wordlist.Length)];
-        drawMan(selection, alphabet, hangs);
-        selectionLength = selection.Length;
-        guessed = "";
-        for (int i = 0; i < selection.Length; i++)
-        {
-            char c = selection[i];
-            if (c == ' ') { guessed += " "; selectionLength--; } else { guessed += "_"; }
+        string answerString = wList[random.Next(0, wList.Length)];
+        answer = answerString.ToCharArray();
+        answerLength = answer.Length;
+        guessedAnswer = new char[answerLength];
+        for (int i = 0; i < answerLength; i++) {
+            if (answer[i] != ' ') { guessedAnswer[i] = '_'; } else { guessedAnswer[i] = answer[i]; }
         }
     }
-    static void setWordList(out string[] wordlist, out char[] letterList, int count)
-    {
-        wordlist = new string[count];
-        for (int i = 0; i < count; i++) { wordlist[i] = getWord(); }
-        letterList = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+    static void SetWordList() {
+        wList = new string[wordListCount];
+        for (int i = 0; i < wordListCount; i++) { wList[i] = GetWord(); }
     }
-    static string getWord()
-    {
+    static string GetWord() {
         string[] word = [
         // coding related
         "strings", "algorithm", "autonomous", "binary numbers", "arrays", "argument", "assignment operators", "java", "javascript", "powershell", "rust", "htmlx", "class", "static", "void", "object"
@@ -70,14 +96,115 @@ class WordGuess {
             // color related
             , "orange", "pink", "almond", "amethyst", "aquamarine", "azure", "aureolin", "beaver", "bittersweet", "bazaar", "black", "blound", "blue", "lime", "bistre", "beige", "bone", "burgundy", "burnt orange", "alice blue", "american rose", "antique brass", "android green", "baby blue", "atomic tangerine", "baby pink", "blizzard blue", "boston university red", "boysenberry", "brick red", "bright lavender", "british racing green", "cadmium green", "cadet grey", "camouflage green", "carrot orange", "carolina blue", "cerulean blue", "cherry blossom pink", "cerise pink", "cornflower", "cornflower blue", "crimson", "crimson glory", "dandelion", "dark candy apple red", "dark jungle green", "dark khaki", "dollar bill", "electric lavender", "electric purple", "ferrari red", "fluorexcent orange", "ghost white"
         ]; // manual word list
-        Random rand = new();
-        var rword = rand.Next(0, word.Length);
-        return word[rword];
-    }
-    static void drawMan(string answer, char[] alphabet, int chances)
-    {
+            Random rand = new();
+            int firstIndex = rand.Next(0, word.Length);
+            string firstWord = word[firstIndex];
 
+            // 50% chance to add a second word
+            if (rand.Next(2) == 0)
+            {
+                int secondIndex;
+                do
+                {
+                    secondIndex = rand.Next(0, word.Length);
+                } while (secondIndex == firstIndex); {
+                string secondWord = word[secondIndex];
+                return firstWord + " " + secondWord; }
+            } else { return firstWord; }
+        
     }
+    static void DrawBoard(char[] answer, char[] alphabet, int chances) {
+        Console.Clear();
+        DrawStand(answer, alphabet, chances);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Guessed letters:");
+        DrawGuessedAnswer(answer);
+        Console.WriteLine();
+        Console.WriteLine("Available letters:");
+        DrawAvailableLetters(alphabet, guessedChars);
+        Console.WriteLine($"Chances left: {hangChances - chances}");
+        Console.WriteLine("Enter a letter to guess:");
+    }
+    static void DrawStand(char[] answer, char[] alphabet, int chances) {
+        foreach (var line in TopGallows) { Console.WriteLine(line); }
+        if (chances >= 1) { Console.WriteLine(BodyParts[1]); }
+        else { Console.WriteLine(BarePost); }
+
+        if (chances == 2) { Console.WriteLine(BodyParts[2]); }
+        else if (chances == 3) { Console.WriteLine(BodyParts[3]); }
+        else if (chances >= 4) { Console.WriteLine(BodyParts[4]); }
+        else { Console.WriteLine(BarePost); }
+        
+        if (chances == 5) { Console.WriteLine(BodyParts[5]); }
+        else { Console.WriteLine(BarePost); }
+        
+        if (chances == 6) { Console.WriteLine(BodyParts[6]); }
+        else if (chances >= 7) { Console.WriteLine(BodyParts[7]); }
+        else { Console.WriteLine(BarePost); }
+        
+        Console.WriteLine(BarePost);
+        Console.WriteLine(BaseGallows);
+    }
+    static void DrawGuessedAnswer(char[] guessed) {
+        // Print the guessed answer, grouping by word and spacing for readability
+        // Example: "_ _ _ _   _ _ _"
+        Console.Write("|| ");
+        for (int i = 0; i < guessed.Length; i++) {
+            if (guessed[i] == ' ') { Console.Write($"   "); }
+            else { Console.Write($"{guessed[i]} "); }
+        }
+        Console.WriteLine("||");
+        Console.WriteLine();
+    }
+    static void DrawAvailableLetters(char[] letters, char[] guessedChars) {
+        // US English letters, split into 8, 10, 8 pattern
+        int[] rowSizes = { 8, 10, 8 };
+        int letterIndex = 0;
+
+        for (int row = 0; row < rowSizes.Length; row++) {
+            for (int col = 0; col < rowSizes[row]; col++)
+            {
+                if (letterIndex >= letters.Length) break;
+                char letter = letters[letterIndex];
+                // If letter has been guessed, show '_', else show the letter
+                if (Array.IndexOf(guessedChars, letter) != -1) { Console.Write("_ "); }
+                else { Console.Write($"{letter} "); }
+                letterIndex++;
+            }
+            Console.WriteLine();
+        }
+    }
+    private static readonly String[] TopGallows = [
+        "  ||======",
+        "  ||    !", 
+        "  ||    ¡", 
+    ];
+    private static readonly String BarePost = "  ||";
+    private static readonly String BaseGallows = "##########";
+    private static readonly Dictionary<int, string> BodyParts = new()
+    {
+        { 1, "  ||    O" }, // Head
+        { 2, "  ||   /" }, // L Arm
+        { 3, "  ||   /\\" }, // Arms
+        { 4, "  ||   /|\\" }, // Armsfull
+        { 5, "  ||    |" }, // Middle
+        { 6, "  ||   /" }, // L Leg
+        { 7, "  ||   / \\" } // Legs
+    };
+    private static bool SetGuessedChars(char letter) {
+        guessedChars ??= [];
+        letter = char.ToLowerInvariant(letter);
+
+        if (Array.IndexOf(guessedChars, letter) != -1) { return false; }
+
+        Array.Resize(ref guessedChars, guessedChars.Length + 1);
+        guessedChars[^1] = letter;
+
+        Array.Sort(guessedChars);
+        return true;
+    }
+
     // char.Parse(Console.ReadLine());
     // Console.WriteLine("");
     // commands for reference
